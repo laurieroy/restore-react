322f5ccba14dce0606fa8ac4896354a1d5807634
lauretteroy <laurieroy.dev@gmail.com>
2021-12-17 10:02:30 -0800

    Add contact page

--- src/features/contact/ContactPage.tsx
+++ src/features/contact/ContactPage.tsx
@@ -1,6 +1,6 @@
 import { Typography } from "@mui/material";
 
-export default function ContactPage {
+export default function ContactPage() {
 	return (
 		<Typography variant='h2'>Contact page</Typography>
 	)
