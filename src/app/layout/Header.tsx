eb53dcb588e4b884e63f9ab4d96bc9e2259024b9
lauretteroy <laurieroy.dev@gmail.com>
2021-12-23 21:30:54 -0800

    Add material ui icons fixing shopping cart

--- src/app/layout/Header.tsx
+++ src/app/layout/Header.tsx
@@ -1,5 +1,5 @@
 import { AppBar, Badge, Box, IconButton, List, ListItem, Switch, Toolbar, Typography } from "@mui/material";
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import { ShoppingCart } from "@mui/icons-material";
 import React from "react";
 import { NavLink } from "react-router-dom";
 
@@ -10,13 +10,13 @@ interface Props {
 
 const midLinks = [
 	{title: 'catalog', path: '/catalog'},
-	{title: 'about', path: '/about'}.
+	{title: 'about', path: '/about'},
 	{title: 'contact', path: '/contact'}
 ]
 
 const rightLinks = [
 	{title: 'login', path: '/login'},
-	{title: 'register', path: '/register'}.
+	{title: 'register', path: '/register'}
 ]
 
 const navStyles= {
@@ -47,14 +47,14 @@ export default function Header({darkMode, handleThemeChange}: Props) {
 				<Box display='flex' alignItems='center'>
 					<IconButton size='large' sx={{color: 'inherit' }}>
 						<Badge badgeContent={4} color='secondary' >
-							<ShoppingCartIcon />
+							<ShoppingCart />
 						</Badge>
 					</IconButton>
 				</Box>
 				<List sx={{display: 'flex' }}>
 					{rightLinks.map(({title, path}) => (
 						<ListItem
-							component={NavLink},
+							component={NavLink}
 							to={path}
 							key={path}
 							sx={navStyles}
