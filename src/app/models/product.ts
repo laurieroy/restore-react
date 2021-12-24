cd89a0512837834b63256d12f193e70b45206a4b
lauretteroy <laurieroy.dev@gmail.com>
2021-12-23 21:02:47 -0800

    Link  Product card button to details, build out

--- src/app/models/product.ts
+++ src/app/models/product.ts
@@ -1,10 +1,31 @@
-export interface Product {
-  id: number;
-  name: string;
-  description: string;
-  price: number;
-  quantityInStock?: number;
-  brand: string;
-  productType?: string;
-  pictureUrl: string;
-}
+// export interface Product {
+//   id: number;
+//   name: string;
+//   description: string;
+//   price: number;
+//   quantityInStock?: number;
+//   brand: string;
+//   productType?: string;
+//   pictureUrl: string;
+// }
+// declare module namespace {
+  export interface Product {
+    id: number;
+    name: string;
+    description: string;
+    price: number;
+    quantityInStock: number;
+    brand: string;
+    productType: string;
+    pictureUrl: string;
+  }
+  export interface Data {
+    id: string;
+    type: string;
+    attributes: Product;
+  }
+
+  // export interface RootObject {
+  //   data: Data;
+  // }
+// }
