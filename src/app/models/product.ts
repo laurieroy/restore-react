19db3a13b715ffd6ecf1f5ad62c8383da0a716ad
lauretteroy <laurieroy.dev@gmail.com>
2022-02-14 19:44:52 -0800

    Adjust to c# api instead of rails

--- src/app/models/product.ts
+++ src/app/models/product.ts
@@ -1,31 +1,32 @@
+export interface Product {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  quantityInStock?: number;
+  brand: string;
+  type?: string;
+  pictureUrl: string;
+}
+
+// declare module namespace {
 // export interface Product {
 //   id: number;
 //   name: string;
 //   description: string;
 //   price: number;
-//   quantityInStock?: number;
+//   quantityInStock: number;
 //   brand: string;
-//   productType?: string;
+//   productType: string;
 //   pictureUrl: string;
 // }
-// declare module namespace {
-  export interface Product {
-    id: number;
-    name: string;
-    description: string;
-    price: number;
-    quantityInStock: number;
-    brand: string;
-    productType: string;
-    pictureUrl: string;
-  }
-  export interface Data {
-    id: string;
-    type: string;
-    attributes: Product;
-  }
+// export interface Data {
+//   id: string;
+//   type: string;
+//   attributes: Product;
+// }
 
-  // export interface RootObject {
-  //   data: Data;
-  // }
+// export interface RootObject {
+//   data: Data;
+// }
 // }
--- src/features/catalog/catalog.tsx
+++ src/features/catalog/catalog.tsx
@@ -6,9 +6,9 @@ export default function Catalog() {
   const [products, setProducts] = useState<Product[]>([]);
 
   useEffect(() => {
-    fetch('http://localhost:3000/api/v1/products')
+    fetch("http://localhost:5000/api/products")
       .then((response) => response.json())
-      .then((data) => setProducts(data.data));
+      .then((data) => setProducts(data));
   }, []);
 
   return (
