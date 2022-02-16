eeb932904ce226d53442dda1b769a7c6d17158e6
lauretteroy <laurieroy.dev@gmail.com>
2022-02-15 20:02:30 -0800

    Add axios interceptor, catch error

--- src/features/about/AboutPage.tsx
+++ src/features/about/AboutPage.tsx
@@ -1,5 +1,58 @@
-import { Typography } from "@mui/material";
+import { Button, ButtonGroup, Container, Typography } from "@mui/material";
+import agent from "../../app/api/agent";
 
 export default function AboutPage() {
-  return <Typography variant="h2">About page</Typography>;
+  return (
+    <Container>
+      <Typography variant="h2" gutterBottom>
+        Errors for testing purposes
+      </Typography>
+      <ButtonGroup fullWidth>
+        <Button
+          variant="contained"
+          onClick={() =>
+            agent.TestErrors.get400Error().catch((error) => console.log)
+          }
+        >
+          Test 400 Error
+        </Button>
+
+        <Button
+          variant="contained"
+          onClick={() =>
+            agent.TestErrors.get401Error().catch((error) => console.log)
+          }
+        >
+          Test 401 Error
+        </Button>
+
+        <Button
+          variant="contained"
+          onClick={() =>
+            agent.TestErrors.get404Error().catch((error) => console.log)
+          }
+        >
+          Test 404 Error
+        </Button>
+
+        <Button
+          variant="contained"
+          onClick={() =>
+            agent.TestErrors.get500Error().catch((error) => console.log)
+          }
+        >
+          Test 500 Error
+        </Button>
+
+        <Button
+          variant="contained"
+          onClick={() =>
+            agent.TestErrors.getValidationError().catch((error) => console.log)
+          }
+        >
+          Test Validation Error
+        </Button>
+      </ButtonGroup>
+    </Container>
+  );
 }
